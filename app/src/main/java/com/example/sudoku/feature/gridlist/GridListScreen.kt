ed293bef4302b7d7e1f02816eb0dcf262d022984
Louis Jacquelin <jacquelinlouis@gmail.com>
2024-01-21 14:54:59 +0100

    Implement GridList

---
+++ app/src/main/java/com/example/sudoku/feature/gridlist/GridListScreen.kt
@@ -0,0 +1,8 @@
+package com.example.sudoku.feature.gridlist
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun GridListScreen() {
+
+}
---
+++ app/src/main/java/com/example/sudoku/feature/gridlist/GridListViewModel.kt
@@ -0,0 +1,9 @@
+package com.example.sudoku.feature.gridlist
+
+import androidx.lifecycle.ViewModel
+
+class GridListViewModel: ViewModel() {
+
+
+
+}

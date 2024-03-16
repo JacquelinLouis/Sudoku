a0833468382eee01350ce9aa522186f459287708
Louis Jacquelin <jacquelinlouis@gmail.com>
2024-03-16 16:43:27 +0100

    Add user digits input

--- app/src/main/java/com/example/sudoku/feature/grid/GridScreen.kt
+++ app/src/main/java/com/example/sudoku/feature/grid/GridScreen.kt
@@ -8,17 +8,26 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Divider
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -49,6 +58,11 @@ fun GridScreen(grid: Grid?) {
         GridComponent(grid)
 }
 
+private val DECIMALS = MutableList(9) { index -> (index + 1).toString() }
+
+private fun String.filterDecimal(): String = takeIf { DECIMALS.contains(it) } ?: ""
+
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 private fun GridComponent(grid: Grid) {
     BoxWithConstraints(
@@ -66,21 +80,21 @@ private fun GridComponent(grid: Grid) {
                                 .fillMaxHeight(),
                             color = if (0 < columnIndex && columnIndex % 3 == 0) Color.Black else Color.LightGray
                         )
-                        TextButton(
-                            modifier = Modifier
-                                .weight(1F)
-                                .aspectRatio(1F),
-                            onClick = { /*TODO*/ }
-                        ) {
-                            Text(
-                                text = column.value.takeIf { it != 0 }?.toString() ?: " ",
+
+                        var text by remember { mutableStateOf(column.value.takeIf { it != 0 }?.toString() ?: "") }
+
+                        TextField(
+                            value = text,
+                            onValueChange = { text = it.filterDecimal() },
+                            enabled = !column.fixed,
+                            readOnly = column.fixed,
+                            modifier = Modifier.weight(1F).fillMaxWidth(),
+                            textStyle = TextStyle(
                                 color = if (column.fixed) Color.Black else Color.LightGray,
-                                textAlign = TextAlign.Center,
-                                modifier = Modifier
-                                    .weight(1F)
-                                    .aspectRatio(1F)
-                            )
-                        }
+                                background = Color.White
+                            ),
+                            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Decimal)
+                        )
                     }
                 }
             }
